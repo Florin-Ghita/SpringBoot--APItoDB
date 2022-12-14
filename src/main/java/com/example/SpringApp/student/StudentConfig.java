ecc501e17a9844e9519042b935334dc301036e7b
Ghita Florin <ghitafm94@gmail.com>
2022-12-15 01:42:43 +0200

    Implemented CRUD methods

---
+++ src/main/java/com/example/SpringApp/student/StudentConfig.java
@@ -0,0 +1,42 @@
+package com.example.SpringApp.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student daniel = new Student(
+
+                    "Daniel",
+                    "daniel@gmail.com",
+                    LocalDate.of(2000, Month.JANUARY,5)
+
+            );
+            Student alex = new Student(
+
+                    "Alex",
+                    "alex@gmail.com",
+                    LocalDate.of(2004, Month.JANUARY,5)
+
+            );
+
+            Student mark = new Student(
+
+                    "Mark",
+                    "Mark@gmail.com",
+                    LocalDate.of(2004, Month.JANUARY,5)
+                    );
+
+    repository.saveAll( List.of(mark,alex,daniel));
+        };
+    }
+}
